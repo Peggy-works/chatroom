2a144094c848b9ce0f82504ff78a8373e973e6eb
Peggys-jpg <55251613+Peggys-jpg@users.noreply.github.com>
2023-04-12 22:52:46 -0700

    Add files via upload

--- client.py
+++ client.py
@@ -1,81 +1,5 @@
-import tkinter as tk
-from datetime import datetime
-from enum import Enum
-from tkinter import ttk
-from socket import * 
-import pickle
-
-class Count:
-    def __init__(self):
-        self.counter = 1.0 
-    
-    def increment(self):
-        self.counter += 1.0
-
-    def incrementBy(self, counter):
-        self.counter += counter
-
-    def get_counter(self):
-        return self.counter
-    
-class User():
-    def __init__(self):  
-        self.username = None  
-        self.color = None
-        self.enabled = False
-        self.state = 'normal'
-
-    def set_state(self, choice):
-        if choice == 1:
-            self.state = 'report'
-        elif choice == 2:
-            self.state = 'request'
-        elif choice == 3:
-            self.state = 'quit' 
-
-    def set_username(self, username):
-        self.username = username
-    
-    def get_state(self):
-        return self.state
-     
-
-
-class Chat():
-    def __init__(self, text):
-        self.text = text
-        self.counter = Count()
-    
-    def displayMenu(self): 
-        self.text.configure(state='normal')
-        self.text.insert(self.counter.get_counter(), 
-    '''Please select one of the following options:
-        1. Get a report of the chatroom from the server.
-        2. Request to join the chatroom.
-        3. Quit the program.\n''')
-        self.text.configure(state='disabled')
-        self.counter.incrementBy(4.0) 
-
-    def insertText(self, counter, message):
-        self.text.configure(state='normal')
-        self.text.insert(counter, message)
-        self.text.configure(state='disabled')
-        self.counter.increment()
-
-class Connection(): 
-    def __init__(self):
-        #Server connection settings 
-        self.clientSocket = socket(AF_INET, SOCK_STREAM)
-        #self.clientSocket = socket(AF_INET, SOCK_STREAM)
-        #self.clientSocket.connect((serverName, serverPort))
-    
-    def connect(self, serverName, serverPort, username):
-        self.clientSocket.connect('localhost', 12000)
-
-    def close(self):
-        self.clientSocket.close()
-
-    
+import time, sys, pickle
+from socket import *
 
 class Message():   
     REPORT_REQUEST_FLAG = None
@@ -92,96 +16,87 @@ class Message():
     FILENAME = None
     PAYLOAD_LENGTH = None
     PAYLOAD = None
-    
+    def set(self, REPORT_REQUEST, REPORT_RESPONSE, JOIN_REQUEST, JOIN_REJECT, JOIN_ACCEPT, NEW_USER, QUIT_REQUEST, QUIT_ACCEPT, ATTATCHMENT, NUMBER, USERNAME, FILENAME, PAYLOAD_LENGTH, PAYLOAD):
+        self.REPORT_REQUEST_FLAG = REPORT_REQUEST
+        self.REPORT_RESPONSE_FLAG = REPORT_RESPONSE
+        self.JOIN_REQUEST_FLAG = JOIN_REQUEST
+        self.JOIN_REJECT_FLAG = JOIN_REJECT
+        self.JOIN_ACCEPT_FLAG = JOIN_ACCEPT
+        self.NEW_USER_FLAG = NEW_USER
+        self.QUIT_REQUEST_FLAG = QUIT_REQUEST
+        self.QUIT_ACCEPT_FLAG = QUIT_ACCEPT
+        self.ATTATCHMENT_FLAG = ATTATCHMENT
+        self.NUMBER = NUMBER
+        self.USERNAME = USERNAME
+        self.FILENAME = FILENAME
+        self.PAYLOAD_LENGTH = PAYLOAD_LENGTH
+        self.PAYLOAD = PAYLOAD
 
-def userinput(event, chat, user, connection): 
-    #print(text.get('2.0', '2.end'))
-    #print(entrybox.get())
-    try: 
-        input = int(entrybox.get())
-        user.set_state(input) 
-        chat.insertText(chat.counter.get_counter(), f'Your choice: {input}\n')
-        entrybox.delete(0, 'end') 
-        if input == 2:
-            chat.text.configure(state='normal')
-            chat.text.insert(chat.counter.get_counter(), f'Please enter username: ')
-            chat.text.configure(state='disabled')
-    except ValueError: 
-        input = entrybox.get()
-        if user.get_state() == 'request':
-            chat.insertText('end', f'{input}\n') 
-            entrybox.delete(0, 'end')
-            message = Message()
-            message.REPORT_REQUEST_FLAG = 0
-            message.REPORT_RESPONSE_FLAG = 0
-            #message.
+def connection():
+    message = Message()  
+    while True:
+        print("""Please select one of the following options:
+    1. Get a report of the chatroom from the server.
+    2. Request to join the chatroom.
+    3. Quit the program.
+    """) 
+        
+        user_input = input("Your choice: ") 
+        if user_input == '1' or user_input == 1:
+            pass
+        if user_input == '2' or user_input == 2: 
+            # User input 
+            username = input("Please enter a username: ")
+            
+            # Setting message, pickling, sending
+            message.set(0, 0, 1, 0, 0, 1, 0, 0, 0, 0, username, "", 0, 0) 
+            data_send = pickle.dumps(message)
+            clientSocket.send(data_send)
+
+            # Server Response
+            response = clientSocket.recv(1024)
+            data_response = pickle.loads(response)
+
+            # Server accept, exit loop 
+            if data_response.JOIN_ACCEPT_FLAG:
+                print("We breaking")
+                break
+            # Server reject, stay in loop
+            elif data_response.JOIN_REJECT_FLAG:
+                print(data_response.PAYLOAD) 
+        elif user_input == '3' or user_input == 3:
+            sys.exit()
+        # Since user is accepted we can now send message to the server indefinitely, except for some conditions
+        #while True:
+            #sentence = input("input lowercase sentence:")
+            #clientSocket.send(sentence.encode())
+    while True:
+        sentence = input("Insert text: ")
+        clientSocket.send(sentence.encode())
+            
+
+            
+
+# Host, port
+HOST = 'localhost'
+PORT = 12000 
+
+#user input
+#user_input = None
+#username = None
  
-     
-    
-    
+# Connecting to server
+clientSocket = socket(AF_INET, SOCK_STREAM)
+clientSocket.connect((HOST, PORT))
 
-def insertText(text, string, index): 
-    text.insert(index, string)
-    index += 1.0
-    return index 
- 
-#Setting room title, dimensions, configuring column/row 
-root = tk.Tk()
-root.title("Chatroom")
-root.geometry('600x200') 
-root.columnconfigure(0, weight=1)
-root.rowconfigure(0, weight=1)   
-
-"""
-This is the frame before main frame
-Users here will be prompted with 3 options to choose from:
-    1. Get a report of the chatroom from the server
-    2. Request to join the chatroom
-    3. Quit the program
-"""
- 
-#This is the main frame(chatroom) that user will interact 
-masterframe = ttk.Frame(root)
-#masterframe = tk.Frame(root, padx=5, pady= 5, bg='black')
-masterframe.grid(column=0, row=0, sticky=('N', 'W,' 'E', 'S')) 
-
-#configuring masterframe column/row
-masterframe.columnconfigure(0, weight=1) 
-masterframe.rowconfigure(1, weight=1) 
- 
-#Chatbox textbox that is disabled(user interaction disabled) 
-chatbox = tk.Text(masterframe, height=25, width=25, bg='black', fg='#49ff38', font=("Helvetica", 10), state='disabled')
-chatbox.grid(column=0, columnspan=2, row=1, sticky=('N', 'W,' 'E', 'S')) 
- 
-
-#User object
-user = User()
-
-#Chatroom object
-chat = Chat(chatbox)
-
-#Message object
-message = Message()
-
-#Connection object
-connection = Connection()
-#connection = Connection(serverName='localhost', serverPort=12000)
+connection()
 
 
-chat.displayMenu() 
- 
 
-#Entry textbook where users insert messages they want sent
-entrybox = tk.Entry(masterframe, width=25)
-entrybox.grid(column=0, row=2, sticky=('N', 'W', 'E', 'S')) 
-entrybox.bind('<Return>', lambda event, chat=chat, user=user, connection=connection: userinput(event, chat, user, connection))  
-
-
-#Enter eventloop to allow users to interact
-root.mainloop()  
-
-"""sentence = input('Input lowercase sentence:')
-connection.clientSocket.send(sentence.encode())
-modifiedSentence = connection.clientSocket.recv(1024)
-print(f'From server: {modifiedSentence.decode()}')
-connection.close()"""
+#message = Message() 
+#while True:
+    #sentence = input("input lowercase sentence:")
+    #clientSocket.send(sentence.encode())
+    #modifiedSentence = clientSocket.recv(1024)
+    #print(f'From server: {modifiedSentence.decode()}')
+clientSocket.close()
--- server.py
+++ server.py
@@ -1,18 +1,120 @@
-from datetime import datetime
+import time, sys, pickle, time
 from socket import *
- 
-serverPort = 12000
-serverName = 'localhost'
+
+class Message():   
+    REPORT_REQUEST_FLAG = None
+    REPORT_RESPONSE_FLAG = None
+    JOIN_REQUEST_FLAG = None
+    JOIN_REJECT_FLAG = None
+    JOIN_ACCEPT_FLAG = None
+    NEW_USER_FLAG = None
+    QUIT_REQUEST_FLAG = None
+    QUIT_ACCEPT_FLAG = None
+    ATTATCHMENT_FLAG = None
+    NUMBER = None
+    USERNAME = None
+    FILENAME = None
+    PAYLOAD_LENGTH = None
+    PAYLOAD = None
+    def set(self, REPORT_REQUEST, REPORT_RESPONSE, JOIN_REQUEST, JOIN_REJECT, JOIN_ACCEPT, NEW_USER, QUIT_REQUEST, QUIT_ACCEPT, ATTATCHMENT, NUMBER, USERNAME, FILENAME, PAYLOAD_LENGTH, PAYLOAD):
+        self.REPORT_REQUEST_FLAG = REPORT_REQUEST
+        self.REPORT_RESPONSE_FLAG = REPORT_RESPONSE
+        self.JOIN_REQUEST_FLAG = JOIN_REQUEST
+        self.JOIN_REJECT_FLAG = JOIN_REJECT
+        self.JOIN_ACCEPT_FLAG = JOIN_ACCEPT
+        self.NEW_USER_FLAG = NEW_USER
+        self.QUIT_REQUEST_FLAG = QUIT_REQUEST
+        self.QUIT_ACCEPT_FLAG = QUIT_ACCEPT
+        self.ATTATCHMENT_FLAG = ATTATCHMENT
+        self.NUMBER = NUMBER
+        self.USERNAME = USERNAME
+        self.FILENAME = FILENAME
+        self.PAYLOAD_LENGTH = PAYLOAD_LENGTH
+        self.PAYLOAD = PAYLOAD
+
+def handleUser(connection, addr, users, user_count):
+    print("In handleUser")
+    message = Message()
+    # Reject if 3 active users
+    if user_count == 3:
+        message.set(0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, "", 0, 'The server rejects the join request')
+        data_send = pickle.dumps(message)
+        connection.send(data_send)
+        #time.sleep(30)
+        #connection.close()
+        return
+    elif user_count < 3:
+        response = connection.recv(1024)
+        data = pickle.loads(response)
+        #print(data.USERNAME)
+        result = users.get(data.USERNAME)
+        if not result:
+            users[data.USERNAME] = {
+                "IP": addr[0],
+                "PORT": addr[1],
+                "SOCKET": connection
+            }
+            user_count += 1
+            username.append(data.USERNAME)
+            message.set(0, 0, 0, 0, 1, 0, 0, 0, 0, 0, data.USERNAME, "", 0, "USER has entered")
+            data_send = pickle.dumps(message)
+            connection.send(data_send)
+        else:
+            message.set(0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0 , "", 0, "The server rejects the join request. Another user is using this username")
+            data_send = pickle.dumps(message)
+            connection.send(data_send)
+    #print(users)
+
+HOST = 'localhost'
+PORT = 12000  
+
+users = {}
+username = []
+user_count = 0
+
 serverSocket = socket(AF_INET, SOCK_STREAM)
-serverSocket.bind((serverName, serverPort))
-serverSocket.listen(1)
-print("The server is ready to recieve")
+serverSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+serverSocket.bind((HOST, PORT))
+serverSocket.listen(3)
+#print("The server is ready to recieve")
+#print(f'User count: {len(users)}')
+#connectionSocket, addr = serverSocket.accept()
+#handleUser(connectionSocket, addr, users, user_count)
 while True:
+    # Accepting new user connection and checking if username is taken
     connectionSocket, addr = serverSocket.accept()
-    sentence = connectionSocket.recv(1024).decode()
-    capitalizedSentence = sentence.upper()
-    connectionSocket.send(capitalizedSentence.encode())
-    connectionSocket.close()
-    print("server close")
+    if connectionSocket:
+        print(f'connectionSocket has something')
+        print(connectionSocket)
+    handleUser(connectionSocket, addr, users, user_count)
+    # if condition for 1,2,3 users
+    if len(users) == 1:
+        print(users[username[0]]['SOCKET'].recv(1024).decode())
+        #quit()
+        pass
+    elif len(users) == 2:
+        print(users[username[0]]['SOCKET'].recv(1024).decode())
+        print(users[username[1]]['SOCKET'].recv(1024).decode())
+        pass
+    elif len(users) == 3:
+        pass
+    """for user in users: 
+        #print(users[user]['IP'])
+        #print(users[user]['PORT'])
+        #print(users[user]['Socket'])
+        resp_message = users[user]['SOCKET'].recv(1024).decode()
+        print(resp_message)"""
+    #print(users)
+    #print(len(users))
+    #print(users)
 
-
+        #users[addr[len(users)]] = 
+    #print(f'Printing connectionSocket: {connectionSocket}')
+    #print(f'Printing address: {addr[1]}')
+    #response = connectionSocket.recv(1024).decode()
+    #print(response)
+    #capitalizedSentence = sentence.upper()
+    #connectionSocket.send(capitalizedSentence.encode())
+    #connectionSocket.close()
+    
+    #print("server close")
